d7b3db3a65bf6aaa9835b075432368992812cabb
Maya Kazakova <anais.maya@gmail.com>
2024-12-18 12:31:49 +0100

    fixed hero img

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,9 @@
-import { HeroSection, HeroContent, HeroButton } from './Hero.styled';
+import { HeroSection, HeroContent, HeroButton, HeroImage } from './Hero.styled';
 
 const Hero = () => {
   return (
     <HeroSection>
+      <HeroImage /> 
       <HeroContent>
         <h1>Campers of your dreams</h1>
         <p>You can find everything you want in our catalog</p>
--- src/components/Hero/Hero.styled.js
+++ src/components/Hero/Hero.styled.js
@@ -2,21 +2,33 @@ import styled from 'styled-components';
 import heroImage from '../../assets/images/heroImg.jpg';
 
 export const HeroSection = styled.section`
-  background-image: url(${heroImage});
-  background-size: cover;
-  background-position: center;
-  height: 100vh;
+    height: 100vh;
   display: flex;
   align-items: center;
   justify-content: flex-start;
   position: relative;
   color: #fff;
-  padding-left: 40px; 
+  padding-left: 40px;
+  overflow: hidden;
 `;
 
-export const HeroContent = styled.div`
+export const HeroImage = styled.div`
+  background-image: url(${heroImage});
+  background-size: cover;
+  background-position: center;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: scaleX(-1); /* Поворачивает только фон */
+  transform-origin: center center;
+`;
+
+  export const HeroContent = styled.div`
   text-align: left;
- 
+  z-index: 1;
+
   h1 {
     font-size: 3rem;
     font-weight: 600;
@@ -37,6 +49,7 @@ export const HeroButton = styled.button`
   font-weight: 500;
   cursor: pointer;
   border-radius: 200px;
+  margin-top: 40px;
 
   &:hover {
     background-color: #e04344;
